1dc9bf68b3a6bdb90c525909f2a41f927f70b909
Raihan <raihannady@gmail.com>
2024-03-02 11:55:41 +0700

    assignment 1

--- assignment/task-1/main.go
+++ assignment/task-1/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	var students = []map[string]string{
+		{"name": "John", "address": "Cimahi", "jobs": "developer", "detail": "Golang is good for concurrency and performance."},
+		{"name": "Alice", "address": "Cimahi", "jobs": "developer", "detail": "Golang provides a clean and efficient syntax."},
+		{"name": "Bob", "address": "Cimahi", "jobs": "developer", "detail": "Prefers Golang for its simplicity and fast compilation."},
+		{"name": "Charlie", "address": "Cimahi", "jobs": "developer", "detail": "Enjoys Golang's strong community support and documentation."},
+		{"name": "David", "address": "Cimahi", "jobs": "developer", "detail": "Finds Golang suitable for building scalable and robust systems."},
+	}
+
+	index, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Invalid index:", os.Args[1])
+		return
+	}
+
+	if index < 0 || index >= len(students)+1 {
+		fmt.Println("Index out of range.")
+		return
+	}
+
+	student := students[index-1]
+	fmt.Printf("No: %d\nName: %s\nAddress: %s\nJobs: %s\nWhy Choose Golang?: %s\n", index, student["name"], student["address"], student["jobs"], student["detail"])
 }
